e930abd0d7996a86fee8031aa722c5b403264c8a
rajeevranjan20 <106504730+rajeevranjan20@users.noreply.github.com>
2024-04-21 01:24:51 +0530

    fourth

---
+++ hw4.py
@@ -0,0 +1,5 @@
+# Area of triangle
+b=eval(input("Enter the value of base:"))
+h=eval(input("Enter the value of height:"))
+area=0.5*b*h
+print(f"The Area of triangle with height {h} and base{b}:{area:.2f}")
