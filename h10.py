f6b9ef94548acec2f4b4ef7d5cad39d62a427c77
rajeevranjan20 <106504730+rajeevranjan20@users.noreply.github.com>
2024-05-09 03:29:15 +0530

    pr

--- h10.py
+++ h10.py
@@ -1,18 +1,21 @@
 
-a=[16,17,4,3,5,2]
+a=[16,17,6,5,2]
 r=[]
 
 n=len(a)
+max=0
+max_index=None
 
-for i in range(len(a)):
-    max=a[i]
-    max_index=i
-    for j in range(max_index+1,len(a),1):
-         
-         if a[j]>=max:
+for i in range(n):
+    
+    if a[i]>max:
+         max=a[i]
+         max_index=i
+    for j in range(max_index+1,len(a)):
+         max=a[i]
+         if a[j]>max:
            # max=a[j]
-            r.append(a[j])
-         else:
-             pass
-
+              r.append(a[j]) 
+              max_index=j   
+r.append(a[n-1])
 print(r)         
